b882feb664e94dd9ffc46b2ead3bccaeab719671
Chanwu Oh <ohchanwu@pm.me>
2025-06-05 13:21:12 +0900

    bulkedit

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,14 @@
 const musicPlayer = document.getElementById('musicPlayer');
 const musicLine = document.querySelector('.musicLine');
+const audio = document.querySelector('audio');
+let isPlaying = false;
 
 musicPlayer.addEventListener('click', () => {
     musicLine.classList.toggle('show');
+    if (isPlaying) {
+        audio.pause();
+    } else {
+        audio.play();
+    }
+    isPlaying = !isPlaying;
 });
