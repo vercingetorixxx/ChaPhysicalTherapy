e4e66b88029bfd8e51260bf39f30c41dd5493127
Chanwu Oh <ohchanwu@pm.me>
2025-06-10 22:34:37 +0900

    bulkedit

--- JS/hamburger.js
+++ JS/hamburger.js
@@ -12,6 +12,7 @@ const hamToggle = document.getElementById('hamToggle');
 const navigation = document.querySelector('.navigation');
 const hamburger = document.querySelector('.hamburger');
 const hamIcon = document.querySelector('.hamIcon');
+const logoContainer = document.querySelector('.logoContainer');
 
 hamToggle.addEventListener('click', () => {
     navigation.classList.toggle('hamBg');
@@ -20,9 +21,11 @@ hamToggle.addEventListener('click', () => {
     if (hamburger.classList.contains('show')) {
         hamburger.classList.remove('show');
         hamIcon.classList.remove('checked');
+        logoContainer.classList.remove('show');
     } else {
         hamburger.classList.add('show');
         hamIcon.classList.add('checked');
+        logoContainer.classList.add('show');
     };
 });
 
@@ -49,9 +52,13 @@ const threshold = window.innerHeight / 10;
 function handleScrollDown() {
     if (window.scrollY > threshold) {
         navigation.classList.add('bg');
+        logoContainer.classList.add('bg');
+        hamIcon.classList.add('bg');
     }
     else {
         navigation.classList.remove('bg');
+        logoContainer.classList.remove('bg');
+        hamIcon.classList.remove('bg');
     }
 }
 
