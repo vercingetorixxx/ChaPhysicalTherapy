a8aef098723851d5c96f58005f4af53f5d2c27fa
Chanwu Oh <ohchanwu@pm.me>
2025-06-10 12:18:33 +0900

    bulkedit

--- JS/script.js
+++ JS/script.js
@@ -40,3 +40,33 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 5000); // Wait 5 seconds to allow loading
     });
 });
+
+
+
+// Dynamic InfoBoxes
+
+document.addEventListener("DOMContentLoaded", () => {
+  const targets = document.querySelectorAll(".infoBox");
+
+  const callback = (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("scrolled");
+      } else{
+        entry.target.classList.remove("scrolled");
+      }
+    });
+  };
+
+  const options = {
+    root: null, // Use viewport as root
+    rootMargin: "0px", // Margin around root
+    threshold: 0.3 // Trigger when 10% of the target is visible
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+
+  targets.forEach(target => {
+    observer.observe(target);
+  });
+});
