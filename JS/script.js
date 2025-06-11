ed750d098ffe0d82023eb32bdb937b6767223f26
Chanwu Oh <ohchanwu@pm.me>
2025-06-11 13:21:28 +0900

    autorefresh remove

--- JS/script.js
+++ JS/script.js
@@ -18,30 +18,6 @@ musicPlayer.addEventListener('click', () => {
 
 
 
-// Video Error Refresh Fix - Cache-Busting doesn't seem to work
-
-document.addEventListener('DOMContentLoaded', () => {
-    const videos = document.querySelectorAll('video');
-
-    // Detect errors during loading
-    videos.forEach(video => {
-        video.addEventListener('error', () => {
-            console.log('Video failed to load. Refreshing page...');
-            location.reload();
-        });
-        
-        // Check if the video is ready after a timeout
-        setTimeout(() => {
-            if (video.readyState < 3) { // Less than 3 means not enough data to play
-                console.log('Video not ready. Refreshing page...');
-                location.reload();
-            }
-        }, 5000); // Wait 5 seconds to allow loading
-    });
-});
-
-
-
 // Dynamic InfoBoxes
 
 document.addEventListener("DOMContentLoaded", () => {
